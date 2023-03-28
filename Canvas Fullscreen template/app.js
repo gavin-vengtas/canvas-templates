7310f6bc81e8887df1ebbf4a11d9f52528d24cce
GVengtas <vengtasgc@gmail.com>
2023-03-28 17:01:10 +0200

    init commit

---
+++ Canvas Fullscreen template/app.js
@@ -0,0 +1,11 @@
+window.addEventListener("load",()=>{
+    const canvas = document.getElementById("game");
+    const c = canvas.getContext('2d');
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    
+
+    
+});
